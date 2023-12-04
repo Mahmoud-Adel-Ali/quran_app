25cb988d8da48229c33f24dae12ec4ae83532423
Mahmoud Adel <mahmoudadela413@gmal.com>
2023-12-03 19:21:34 -0800

    create bottom container to player

--- lib/views/widgets/readerListView.dart
+++ lib/views/widgets/readerListView.dart
@@ -1,4 +1,3 @@
-
 import 'package:flutter/material.dart';
 import 'package:quran_app/model/reader.dart';
 
@@ -12,63 +11,100 @@ class ReaderListView extends StatefulWidget {
 }
 
 class _ReaderListViewState extends State<ReaderListView> {
-  bool onPressed = false;
+  bool isPlay = false;
+  bool showBottomContainer = false;
+  
   @override
   Widget build(BuildContext context) {
-    return SizedBox(
-      height: MediaQuery.of(context).size.height - 240,
-      child: ListView.builder(
-          itemCount: widget.reader.content.length,
-          itemBuilder: (context, index) {
-            return GestureDetector(
-              onTap: () {
-                showModalBottomSheet(
-                  context: context,
-                  builder: ((context) {
-                    return Container(
-                      decoration: const BoxDecoration(
-                          image: DecorationImage(
-                            image: AssetImage(
-                              "assets/img/makka.jpg",
-                            ),
-                          ),
-                          ),
-                      child: Column(
-                        children: [
-                          SizedBox(height: 10),
-                          Text(
-                            widget.reader.name,
-                            style:
-                                TextStyle(color: Colors.black, fontSize: 27,fontWeight: FontWeight.bold),
-                          ),
-                          SizedBox(height: 5),
-                        ],
+    return Column(
+      children: [
+        SizedBox(
+          height: MediaQuery.of(context).size.height - 360,
+          child: ListView.builder(
+              itemCount: widget.reader.content.length,
+              itemBuilder: (context, index) {
+                return GestureDetector(
+                  onTap: () {
+                    setState(() {
+                      showBottomContainer = true;
+                    });
+                  },
+                  child: Container(
+                    alignment: Alignment.centerRight,
+                    margin: const EdgeInsets.all(5),
+                    height: 50,
+                    width: double.infinity,
+                    decoration: const BoxDecoration(
+                      border: Border(
+                        bottom: BorderSide(
+                          width: 5,
+                          color: Colors.white,
+                        ),
                       ),
-                    );
-                  }),
-                );
-              },
-              child: Container(
-                alignment: Alignment.centerRight,
-                margin: const EdgeInsets.all(5),
-                height: 50,
-                width: double.infinity,
-                decoration: const BoxDecoration(
-                  border: Border(
-                    bottom: BorderSide(
-                      width: 5,
-                      color: Colors.white,
+                    ),
+                    child: Text(
+                      widget.reader.content[index],
+                      style: const TextStyle(
+                          fontSize: 22, fontWeight: FontWeight.w800),
                     ),
                   ),
-                ),
-                child: Text(
-                  widget.reader.content[index],
-                  style: const TextStyle(
-                      fontSize: 22, fontWeight: FontWeight.w800),
-                ),
+                );
+              }),
+        ),
+         showBottomContainer ? Container(
+          
+          child: Column( 
+            children: [
+              const SizedBox(height: 10),
+              
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: [
+                  const Text("00:00"),
+                  Slider(value: 1.0, onChanged: (value) {}),
+                  const Text("22:10"),
+                ],
               ),
-            );
-          }),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: [
+                  IconButton(
+                      onPressed: () {
+                        setState(() {
+                          isPlay = !isPlay;
+                        });
+                        
+                      },
+                      icon: const Icon(
+                        Icons.first_page,
+                        size: 33,
+                        
+                      )),
+                  IconButton(
+                      onPressed: () {
+                        setState(() {
+                          isPlay = !isPlay;
+                        });
+                      },
+                      icon: Icon(
+                        isPlay ? Icons.play_arrow : Icons.stop,
+                        size: 33,
+                        
+                      )),
+                  IconButton(
+                      onPressed: () {},
+                      icon: const Icon(
+                        Icons.last_page,
+                        size: 33,
+                        
+                      )),
+                ],
+              ),
+            ],
+          ),
+        )
+        :Text(""),
+      ],
     );
   }
 }
